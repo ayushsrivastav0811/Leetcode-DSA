86043c1e017c9a8921c67297c7749917cd446d02
Ayush Srivastav <61233311+ayushsrivastav0811@users.noreply.github.com>
2022-05-24 00:53:55 +0530

    Update Set Matrix Zeroes.cpp

--- Day 1: Arrays/Set Matrix Zeroes.cpp
+++ Day 1: Arrays/Set Matrix Zeroes.cpp
@@ -36,3 +36,70 @@ public:
         // return matrix;
     }
 };
+
+// More optimal using the two dummy array 
+// time complexity:O(N*M + N*M)
+// space complexity:O(N)+O(M)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        vector<int>rowwise(row,-1);
+        vector<int>colwise(col,-1);
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    rowwise[i]=0;
+                    colwise[j]=0;
+                }
+            }
+        }
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(rowwise[i]==0 || colwise[j]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+};
+3->Most optimal
+// time complexity:O(N*M + N*M)
+// space complexity:O(1)
+class Solution {
+	public:
+		void setZeroes(vector<vector<int>>& matrix) {
+			int row=matrix.size();
+			int col=matrix[0].size();
+			bool column=true;
+			for(int i=0; i<row; i++) {
+				if(matrix[i][0]==0)
+					column=false;
+				for(int j=1; j<col; j++) {
+					if(matrix[i][j]==0) {
+						matrix[0][j]=0;
+						matrix[i][0]=0;
+					}
+				}
+			}
+			// traverse from the back side
+			for(int i=row-1; i>=0; i--) {
+				for(int j=col-1; j>=1; j--) {
+					if(matrix[i][0]==0 || matrix[0][j]==0) {
+						matrix[i][j]=0;
+
+					}
+
+				}
+				if(column==false)
+					matrix[i][0]=0;
+			}
+		}
+};

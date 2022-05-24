da920aedb0c4f891fc478956180584eb4883297e
Ayush Srivastav <61233311+ayushsrivastav0811@users.noreply.github.com>
2022-05-25 01:28:17 +0530

    Update Sort Colors.cpp

--- Day 1: Arrays/Sort Colors.cpp
+++ Day 1: Arrays/Sort Colors.cpp
@@ -5,7 +5,7 @@ sol 3.
 public:
     void sortColors(vector<int>& nums) {
         int low=0,mid=0,high=nums.size()-1;
-         while(mid<high){
+         while(mid<=high){
              if(nums[mid]==0)
              {
                  swap(nums[mid],nums[low]);
